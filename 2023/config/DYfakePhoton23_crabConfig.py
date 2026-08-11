6950ed28ac6ba16c03924f103333fcfbff3eaca5
jhlee-003 <jhl003@korea.ac.kr>
2026-08-11 19:22:10 +0900

    Update DYfakePhoton23_crabConfig.py

--- 2023/config/DYfakePhoton23_crabConfig.py
+++ 2023/config/DYfakePhoton23_crabConfig.py
@@ -9,17 +9,14 @@ base = "DY_FakePhoton_Run3_23_FullSim"
 n = 1
 while True:
     req = "{}{}".format(base, n)
-    # CRAB creates crab_<requestName>
     path1 = os.path.join(work_area, "crab_{}".format(req))
     if not os.path.exists(path1):
         break
     n += 1
 
-config.section_("General")                  #
-config.General.requestName = req            #
-config.General.workArea = work_area         #
-config.General.transferLogs = True          #
-config.General.transferOutputs = True       #
+config.section_("General")
+config.General.requestName = req
+config.General.workArea = work_area
 
 config.section_("Debug")
 config.section_("JobType")
@@ -30,20 +27,32 @@ config.JobType.numCores = 2
 config.JobType.maxMemoryMB = 5000
 config.JobType.maxJobRuntimeMin = 900
 config.Debug.extraJDL = ["request_disk = 8000000"]
-config.JobType.inputFiles  = ["voms_proxy.txt", "config/DYfakePhoton23_FullSim.env", "config/DYfakePhoton23_fragment.py", "ProduceDYfakePhoton23.sh"]
-config.JobType.outputFiles = ["DYto2L-2Jets_MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8__Run3Summer23NanoAODv12-130X_mcRun3_2023_realistic_v14__privateProduction__job.root"]
+config.JobType.inputFiles  = [
+    "voms_proxy.txt",
+    "config/DYfakePhoton23_FullSim.env",
+    "config/DYfakePhoton23_fragment.py",
+    "ProduceDYfakePhoton23.sh"
+]
+config.JobType.outputFiles = [
+    "DYto2L-2Jets_MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8__Run3Summer23NanoAODv12-130X_mcRun3_2023_realistic_v14__privateProduction__job.root"
+]
 
-# Pass args to run.sh
+config.JobType.scriptArgs  = [
+    "script=ProduceDYfakePhoton23.sh",
+    "events=10000",
+    "names=DYfakePhoton23_FullSim.env"
+]
 
 config.section_("Data")
 config.Data.outputPrimaryDataset = base
+config.Data.publication = False
 config.Data.splitting   = "EventBased"
 config.Data.unitsPerJob = 1
 
-config.JobType.scriptArgs  = ["script=ProduceDYfakePhoton23.sh", "events=10000", "names=DYfakePhoton23_FullSim.env"]
-config.Data.totalUnits  = 10
-
-config.Data.publication = False
-
 config.section_("Site")
-config.Site.storageSite = "T3_KR_KNU"
+
+#-----------Edit Below------------
+config.Data.totalUnits  = 10             # Number of CRAB jobs
+config.Site.storageSite = "T3_KR_KNU"    # Storage site where you have write permission (Required syntactically by CRAB)
+config.General.transferOutputs = False   # Change to `True` if you want to transfer your output to your storage site
+config.General.transferLogs = False      # Change to `True` if you want to transfer the log to your storage site
