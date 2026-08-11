73abcc9828b2ce31a80e5d94f9ce69bf117e6f31
jhlee-003 <jhl003@korea.ac.kr>
2026-08-11 20:53:35 +0900

    Update DYfakePhoton22_crabConfig.py

--- 2022/config/DYfakePhoton22_crabConfig.py
+++ 2022/config/DYfakePhoton22_crabConfig.py
@@ -28,19 +28,13 @@ config.JobType.maxMemoryMB = 5000
 config.JobType.maxJobRuntimeMin = 900
 config.Debug.extraJDL = ["request_disk = 8000000"]
 config.JobType.inputFiles  = [
-    "voms_proxy.txt", 
-    "config/DYfakePhoton22_FullSim.env", 
-    "config/DYfakePhoton22_fragment.py", 
+    "voms_proxy.txt",
+    "config/DYfakePhoton22_FullSim.env",
+    "config/DYfakePhoton22_fragment.py",
     "ProduceDYfakePhoton22.sh"
 ]
 config.JobType.outputFiles = [
     "DYto2L-2Jets_MLL-50_TuneCP5_13p6TeV_amcatnloFXFX-pythia8__Run3Summer22_NanoAODv12-130X_mcRun3_2022_realistic_v5__privateProduction__job.root"
-]
-
-config.JobType.scriptArgs  = [
-    "script=ProduceDYfakePhoton22.sh", 
-    "events=10000", 
-    "names=DYfakePhoton22_FullSim.env"
 ]
 
 config.section_("Data")
@@ -51,8 +45,15 @@ config.Data.unitsPerJob = 1
 
 config.section_("Site")
 
-#-----------Edit Below------------
-config.Data.totalUnits  = 10             # Number of CRAB jobs
+config.JobType.scriptArgs  = [
+    "script=ProduceDYfakePhoton22.sh",
+    "events=10000",
+    "names=DYfakePhoton22_FullSim.env",
+
+#--------------------------Edit Below--------------------------
+    "stageout_dir=YOUR_DIR/2022"         # Your directory name inside htozg-dy-privatemc directory
+]
+config.Data.totalUnits  = 10000          # Number of CRAB jobs
 config.Site.storageSite = "T3_KR_KNU"    # Storage site where you have write permission (Required syntactically by CRAB)
 config.General.transferOutputs = False   # Change to `True` if you want to transfer your output to your storage site
 config.General.transferLogs = False      # Change to `True` if you want to transfer the log to your storage site
